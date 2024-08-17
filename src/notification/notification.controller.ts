43d489a99ac93342bcd74078dd67aff3bb6a5928
AliAfsharnia <mr.ali007021@gmail.com>
2024-08-17 12:22:58 +0330

    ✨ feat : add swagger to project

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('notifications')
 @Controller('notification')
 export class NotificationController {}
--- src/review-and-rating/review-and-rating.controller.ts
+++ src/review-and-rating/review-and-rating.controller.ts
@@ -1,4 +1,6 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("comments")
 @Controller('review-and-rating')
 export class ReviewAndRatingController {}
