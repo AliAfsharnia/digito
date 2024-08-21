bc42ee4577b16a9e6073de194f081e74f1cb8738
AliAfsharnia <mr.ali007021@gmail.com>
2024-08-21 11:52:54 +0330

    🐛fix: no more public jwt_secret :)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/user.entity';
+import { jwtConstants } from './constants';
+
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity]),JwtModule.register({
     global: true,
     secret: jwtConstants.secret,
-    signOptions: { expiresIn: '1h' },
+    signOptions: { expiresIn: '1m' },
   }),],
   providers: [AuthService],
   controllers: [AuthController],
