d4841a719addf95d8572a5dc7b4d567ebc2e8242
AliAfsharnia <aliafsharnia.work@gmail.com>
2024-08-28 19:41:11 +0330

    🗃️feat: move database config to .env file

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,14 @@
 import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const config: PostgresConnectionOptions = {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: '123',    
-    database: 'digito',
+    host: process.env.DATABASE_HOST,    
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,    
+    database: process.env.DATABASE_DATABASE,
     entities: [ __dirname +'/**/*.entity{.ts,.js}'],
     synchronize: false,
     migrations: [ __dirname +'/migrations/**/*{.ts,.js}'],
